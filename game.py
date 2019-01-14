f1ad80b087c87b32b4f8df08496b723f20f6d7a0
alby <albertagoya@gmail.com>
2019-01-15 00:09:13 +0300

    First commit

---
+++ game.py
@@ -0,0 +1,31 @@
+import random
+number=random.randint(1,99)
+
+
+
+def guess():
+    guesses = 0
+    while guesses < 5:
+
+        number_guessed = int(input("Guess a number between 1-99: "))
+
+        if number_guessed < 1:
+            print("The number must not be less than 1")
+        elif number_guessed > 99:
+            print("The number must not be more than 99")
+        else:
+            guesses += 1
+            guesses_rem = 5 - guesses
+            print("You have " + str(guesses_rem) + " Guesses")
+        if number == number_guessed:
+            print("Congratulations, you have found the number" + str(number))
+            print("You guess it in : ", str(guesses) + " guesses")
+            break
+    if number !=number_guessed:
+            print("You loose, the number is "+ str(number))
+
+
+
+
+
+guess()
