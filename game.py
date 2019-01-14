b360491e80043e5e7a7f686ef29fac0393aad72d
alby <albertagoya@gmail.com>
2019-01-15 00:44:08 +0300

    Fifth commit

--- game.py
+++ game.py
@@ -5,7 +5,10 @@ number=random.randint(1,99)
 
 def guess():
     guesses = 0
+
+    print("This is round " + str(round))
     while guesses < 5:
+
 
         number_guessed = int(input("Guess a number between 1-99: "))
 
@@ -18,17 +21,19 @@ def guess():
             guesses_rem = 5 - guesses
             print("You have " + str(guesses_rem) + " Guesses")
         if number == number_guessed:
-            print("Congratulations, you have found the number" + str(number))
+            print("Congratulations, you have found the number ->" + str(number))
             print("You guess it in : ", str(guesses) + " guesses")
             break
     if number !=number_guessed:
+        print("You loose!!! ")
 
-        print("You loose, the number is "+ str(number))
         repeat_prompt= input("Do you want to play again? Click y for yes and n for no: ")
         if repeat_prompt == 'y':
+
             guess()
         else:
             print("You have terminated the game!")
+            print("You loose, the number was " + str(number))
 
 
 
